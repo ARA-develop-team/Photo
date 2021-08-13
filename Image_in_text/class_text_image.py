5fd2d917b0e3194ec65906c5bf9725e0b47e9686
Vova Vlasuk <vlasuk.roma04@gmail.com>
2021-08-13 12:20:08 +0300

    work with class image

--- Image_in_text/class_text_image.py
+++ Image_in_text/class_text_image.py
@@ -5,8 +5,9 @@ class Image(object):
         self.image_in_pix_list = image_in_pix_list
         self.save = []
         self.black_white = False
+        self.average_brightness = 0
 
-    def black_white(self):
+    def make_black_white(self):
         for y in range(len(self.image_in_pix_list)):
             for x in range(len(self.image_in_pix_list[0])):
 
@@ -18,20 +19,72 @@ class Image(object):
                 self.image_in_pix_list[y][x] = (sr, sr, sr)
 
     def high_contrast(self):
+        self.average_brightness = self.get_average_brightness()
         sr = 0
         for y in range(len(self.image_in_pix_list)):
             for x in range(len(self.image_in_pix_list[0])):
-                if sr < 190:
+                if sr < self.average_brightness:
                     sr = 0
                 else:
                     sr = 255
                 self.image_in_pix_list[y][x] = (sr, sr, sr)
 
-    def average_brightness(self):
+    def get_average_brightness(self):
+        brightness = 0
+        for y in range(len(self.image_in_pix_list)):
+            for x in range(len(self.image_in_pix_list[0])):
 
+                r = self.image_in_pix_list[y][x][0]
+                g = self.image_in_pix_list[y][x][1]
+                b = self.image_in_pix_list[y][x][2]
 
+                sr = (r + g + b) // 3
+                brightness += sr
 
-def save(self):
+        return brightness / len(self.image_in_pix_list) + len(self.image_in_pix_list[0])
+
+    def decrease_extension_photo(self, size_x, compression):
+        # compression: 1 - без сжатия; >1 - с сжатием по y; <1 - с разширением по y
+
+        remainder = len(self.image_in_pix_list[0]) % size_x  # остаток
+        for list_y in range(len(self.image_in_pix_list)):
+            for _ in range(remainder):
+                self.image_in_pix_list[list_y].pop()  # удаление лишних рядов пикселей для подальшого деления справа
+
+        a = int(len(self.image_in_pix_list[0]) / size_x)  # шаг с которым будут обэдинятся пиксели по x
+        b = int(a * compression)  # шаг с которым будут обэдинятся пиксели по y
+
+        remainder = len(self.image_in_pix_list) % b  # остаток
+        print(remainder)
+        for _ in range(remainder):
+            self.image_in_pix_list.pop()  # удаление лишних рядов пикселей для подальшого деления снизу
+
+        new_pix_list = []
+        number_y = 0
+
+        # объединение пикселей
+        for y in range(0, len(self.image_in_pix_list), b):
+            new_pix_list.append([])
+            for x in range(0, len(self.image_in_pix_list[0]), a):
+                sum_r = 0  # сума r в rgb всех объединение пикселей
+                sum_g = 0  # сума g в rgb всех объединение пикселей
+                sum_b = 0  # сума b в rgb всех объединение пикселей
+                for x1 in range(a):
+                    for y1 in range(b):
+                        sum_r += self.image_in_pix_list[y + y1][x + x1][0]
+                        sum_g += self.image_in_pix_list[y + y1][x + x1][1]
+                        sum_b += self.image_in_pix_list[y + y1][x + x1][2]
+                # среднее значение
+                sum_r = sum_r / (a * b)
+                sum_g = sum_g / (a * b)
+                sum_b = sum_b / (a * b)
+
+                new_pix_list[number_y].append((sum_r, sum_g, sum_b))  # добавление сжатого пикселя
+            number_y += 1
+
+        self.image_in_pix_list = new_pix_list
+
+    def save(self):
         self.save.append(self.image_in_pix_list)
 
     def load(self, save_number):
--- Image_in_text/main.py
+++ Image_in_text/main.py
@@ -1,5 +1,6 @@
 import pygame
 from PIL import Image
+import class_text_image
 
 
 def create_pix_list(pix):
@@ -11,82 +12,33 @@ def create_pix_list(pix):
     return pix_list
 
 
-def decrease_extension_photo(pix_list, size_x):
-    remainder = len(pix_list[0]) % size_x
-    for list_y in range(len(pix_list)):
-        for _ in range(remainder):
-            pix_list[list_y].pop()
-
-    a = int(len(pix_list[0]) / size_x)
-    b = int(a * 1.6)
-    remainder = len(pix_list) % b
-    print(remainder)
-    for _ in range(remainder):
-        pix_list.pop()
-
-    new_pix_list = []
-    number_y = 0
-    for y in range(0, len(pix_list), b):
-        new_pix_list.append([])
-        for x in range(0, len(pix_list[0]), a):
-            sum = 0
-            for x1 in range(a):
-                for y1 in range(b):
-                    sum += pix_list[y + y1][x + x1][0]
-            sum = sum / (a * b)
-
-            new_pix_list[number_y].append((sum, sum, sum))
-        number_y += 1
-    return new_pix_list
-
-
-def black_and_white_photo(pix_list, ):
-    for y in range(height):
-        for x in range(width):
-
-            r = pix_list[y][x][0]  # узнаём значение красного цвета пикселя
-            g = pix_list[y][x][1]  # зелёного
-            b = pix_list[y][x][2]  # синего
-
-            sr = (r + g + b) // 3  # среднее значение
-            sr = int(sr * 1)
-            if sr < 190:
-                sr = 0
-            else:
-                sr = 255
-
-            if sr > 255:
-                sr = 255
-            pix_list[y][x] = (sr, sr, sr)
-    return pix_list
-
-
-image = Image.open('samolet.jpg')
+image = Image.open('kot.jpg')
 width = image.size[0]
 height = image.size[1]
-
 pix = image.load()
 
-screen_size = [width, height]
+pix_list = create_pix_list(pix)
+image = class_text_image.Image(pix_list)
+
+screen_size = [700, 700]
 screen_color = (0, 0, 0)
 window = pygame.display.set_mode(screen_size)
 
-pix_list = create_pix_list(pix)
-pix_list_bw = black_and_white_photo(pix_list)
-pix_list_pg = pix_list_bw
-pix_list2 = decrease_extension_photo(pix_list_bw, 22)
-
 window.fill(screen_color)
-for y in range(len(pix_list2)):
-    for x in range(len(pix_list2[0])):
-        pygame.draw.circle(window, pix_list2[y][x], (x, y), 1, 1)
 
-print(len(pix_list2))
-print(len(pix_list2[0]))
-for y in range(len(pix_list2)):
+image.make_black_white()
+image.high_contrast()
+
+for y in range(len(image.image_in_pix_list)):
+    for x in range(len(image.image_in_pix_list[0])):
+        pygame.draw.circle(window, image.image_in_pix_list[y][x], (x, y), 1, 1)
+
+image.decrease_extension_photo(40, 2)
+
+for y in range(len(image.image_in_pix_list)):
     print('')
-    for x in range(len(pix_list2[0])):
-        col = (pix_list2[y][x][0] + pix_list2[y][x][1] + pix_list2[y][x][2]) / 3
+    for x in range(len(image.image_in_pix_list[0])):
+        col = (image.image_in_pix_list[y][x][0] + image.image_in_pix_list[y][x][1] + image.image_in_pix_list[y][x][2]) / 3
         if col < 51:
             print('─', end='')
         elif col < 102:
